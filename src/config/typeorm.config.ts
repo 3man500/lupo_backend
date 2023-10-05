531b6d3ae57c5cc02b597603d83fd3db09e9d950
박지원 <parkjiwon@bagjiwon-ui-MacBookPro.local>
2023-10-05 18:13:19 +0900

    fix: username을 dbconfig 파일에서 가져오도록 수정

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,7 +7,7 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     type: dbConfig.type,
     host: process.env.HOSTNAME || dbConfig.host,
     port: process.env.PORT || dbConfig.port,
-    username: process.env.USERNAME || dbConfig.username,
+    username: dbConfig.username,
     password: process.env.PASSWORD || dbConfig.password,
     database: process.env.DATABASE || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{ts,js}'],
