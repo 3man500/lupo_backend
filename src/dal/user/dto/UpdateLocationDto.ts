d960752f68581324d6de40f7ba7c6ea55e683206
박지원 <parkjiwon@bagjiwon-ui-MacBookPro.local>
2023-10-10 21:35:36 +0900

    fix: update location dto 수정

--- src/dal/user/dto/UpdateLocationDto.ts
+++ src/dal/user/dto/UpdateLocationDto.ts
@@ -2,8 +2,6 @@ import { IsNotEmpty } from "class-validator"
 import { Double, Long } from "typeorm"
 
 export class UpdateLocationDto{
-    @IsNotEmpty()
-    userId: number
     lat: Double
     lon: Double
 }
